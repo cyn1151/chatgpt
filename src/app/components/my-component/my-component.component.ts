443c6dea1385a64523981f0266d9b39d5c4b66b9
蔡雅宁 <1846107346@qq.com>
2023-07-21 08:38:59 +0800

    init-project

---
+++ src/app/components/my-component/my-component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.scss']
+})
+export class MyComponentComponent {
+//add app-my-component to app.component.html
+  title = 'chatGPT-yc40041';
+      
+
+}
